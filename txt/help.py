580b5f81801f657693297597f3a952786b99558a
Karthikeyan G <karthikeyan2021sch@gmail.com>
2021-08-04 15:04:02 +0530

    Add files via upload

---
+++ txt/help.py
@@ -0,0 +1,131 @@
+help = """
+    Command           Description                      
+    -------           -----------                      
+    help (or) h       Show available commands.        
+    exit (or) e       Exit from the framework                             
+    install (or) c    Install Jovian                    
+    back (or) b       Back to main menu                
+    clear             Clear the screen 
+
+    The usage of this framework are:
+    1) Clone a Notebook hosted on Jovian or GitHub 
+    2) Commit a Notebook to Jovian
+    3) Open a Live Jupyter Server  
+    4) Submit Assignment to Jovian Website   (Under Construction)
+
+    
+    [b] back main menu   [e] Exit script
+"""
+
+Options = """
+ [1] Clone Jovian Notebook
+ [2] Commit to Jovian
+ [3] Clone GitHub Repository 
+ [4] Open a Live Jupyter Server 
+ [5] Submit Assignment (Under Construction)
+
+ [e] Exit script    [h] Help Message    [c] Install Jovian"""
+
+Jovian_Notebook = """
+ [1] Ready! 
+     usage = <Jovian Profile + Notebook> example: karthikeyan-g/ipl-matches-completed
+
+ [b] back main menu    [e] Exit script    [h] Help Message    [c] Install Jovian
+"""
+
+Commit_Repo = """
+ [1] Ready!
+     usage = <path> example: C:\Documents\example.ipynb
+
+ [b] back main menu    [e] Exit script    [h] Help Message    [c] Install Jovian
+"""
+
+GitHub_Repo = '''
+ [1] Ready!
+     usage = <url> example: https://github.com/karthiksuki/Jovian-automation
+
+ [b] back main menu    [e] Exit script    [h] Help Message    [c] Install Jovian
+
+'''
+
+Jupyter_Notebook = """
+ [1] Open On Jupyter Lab             (recommend)
+ [2] Open On Jupyter Notebook
+
+ Note: Folder or File Path is Optional if you leave it as blank then the
+       jupyter lab or notebook will open on the current working directory.
+
+ [b] back main menu    [e] Exit script    [h] Help Message    [c] Install Jovian
+
+"""
+
+Submit_Project = '''
+ [1] Data Analysis with Python: Zero to Pandas
+ [2] Deep Learning with PyTorch: Zero to GANs
+ [3] Data Structures and Algorithms in Python
+ [4] Machine Learning with Python: Zero to GBMs
+     
+
+ [b] back main menu    [e] Exit script    [h] Help Message    [c] Install Jovian
+
+'''
+
+Submit_Assigments_Pandas = '''
+ [1] Assignment 1 - Python Basics Practice
+ [2] Assignment 2 - Numpy Array Operations
+ [3] Assignment 3 - Pandas Practice 
+ [4] Course Project - Exploratory Data Analysis
+
+
+ [b] back main menu    [e] Exit script    [h] Help Message    [c] Install Jovian
+
+'''
+
+Submit_Assigments_Gans = '''
+ [1] Assignment 1 - All About torch.Tensor
+ [2] Assignment 2 - Train Your First Model
+ [3] Assignment 3 - Feed Forward Neural Networks
+ [4] Course Project - Train a Deep Learning Model from Scratch
+
+
+ [b] back main menu    [e] Exit script    [h] Help Message    [c] Install Jovian
+ 
+'''
+
+Submit_Assigments_algo = '''
+ [1] Assignment 1 - Binary Search Practice
+ [2] Assignment 2 - Hash Tables and Python Dictionaries
+ [3] Assignment 3 - Divide and Conquer Practice
+ [4] Project - Step-by-Step Solution to a Programming Problem
+
+
+ [b] back main menu    [e] Exit script    [h] Help Message    [c] Install Jovian 
+
+'''
+
+
+Submit_Assigments_gbms = '''
+ [1] Assignment 1 - Train Your First ML Model
+ [2] Assignment 2 - Decision Trees and Random Forests
+ [3] Course Project - Real-World Machine Learning Model
+ [4] (Optional) Assignment 3 - Gradient Boosting Practice
+
+
+ [b] back main menu    [e] Exit script    [h] Help Message    [c] Install Jovian 
+
+'''
+
+Submit_to_jovian = '''
+ [1] Ready!
+     usage = <url> example: https://jovian.ai/karthikeyan-g/examplenotebook
+
+ [b] back main menu    [e] Exit script    [h] Help Message    [c] Install Jovian
+'''
+
+Aftercode = ('''
+
+
+               [b] back main menu    [e] Exit script    [h] Help Message    [c] Install Jovian
+
+                    '''
+                    )
